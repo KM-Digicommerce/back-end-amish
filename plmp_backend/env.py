2fabca1cf03cc54089e3b2dbce7acde363d5e585
Ubuntu <ubuntu@ip-172-31-47-142.ec2.internal>
2024-11-09 06:10:15 +0000

    Commit local changes to env.py before merging

---
+++ plmp_backend/env.py
@@ -0,0 +1,14 @@
+MONGODB_COURSE_DB_NAME="PLMP"
+MONGODB_HOST_1="mongodb://52.201.29.59:27017" 
+front_end_ip ="http://3.91.73.109:3000/"
+from datetime import timedelta
+SIMPLE_JWT = {
+  'ACCESS_TOKEN_LIFETIME': timedelta(minutes=500),
+  'ALGORITHM': 'HS256',
+  'SIGNING_KEY': 'u22h&79gj6o_q^sd*t(h6lbqc8w!zk!1vbk3b_st$s^97tsn3i',
+  'SESSION_COOKIE_DOMAIN' : '',
+  'SESSION_COOKIE_MAX_AGE' : 120000000000,
+  'AUTH_COOKIE': 'access_token', 
+  'AUTH_COOKIE_SECURE': False,    
+  'AUTH_COOKIE_SAMESITE': 'None', 
+}
